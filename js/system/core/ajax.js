8740ce58ed58748630e0b4a385f05e254423b9c3
Dario D. Müller <dario-domi@hotmail.de>
2016-11-30 19:02:42 +0100

    ADDED ajax Get/Post, send token, send data, error callbacks

--- js/system/core/ajax.js
+++ js/system/core/ajax.js
@@ -1,6 +1,6 @@
 define(function() {
 	return {
-		load: function (url, callback, callbackError) {
+		load: function (isGET, url, callback, callbackError, postData, token) {
 			var xmlHttp = null;
 
 			try {
@@ -18,24 +18,35 @@ define(function() {
 			}
 
 			if (xmlHttp) {
-				xmlHttp.open('GET', url, true);
+				var requestType = isGET ? 'GET' : 'POST';
+				xmlHttp.open(requestType, url, true);
 
    				xmlHttp.addEventListener('load', function(event) {
       				if (xmlHttp.status >= 200 && xmlHttp.status < 300) {
 						callback(xmlHttp.responseText);
 					} else if(callbackError !== undefined) {
-							callbackError();
+						callbackError();
 					}
 				});
 
-				xmlHttp.send(null);
+				var data = null;
+				if(isGET == false && postData != undefined) {
+					data = JSON.stringify(postData);
+					xmlHttp.setRequestHeader('Content-Type', 'application/json');
+				}
+				if(token !== undefined && token !== null)
+					xmlHttp.setRequestHeader('X-CSRF-Token', token);
+				xmlHttp.send(data);
+
 			}
+
 		},
 
-		json: function (url, callback) {
-			this.load(url, function (data) {
-				callback(eval('(' + data + ')'));
-			});
+		json: function (isGET, url, callback, callbackError, postData, token) {
+			var callback2 = function (data) {
+				callback(JSON.parse(data || "{}"));
+			};
+			this.load(isGET, url, callback2, callbackError, postData, token);
 		}
 	};
 });
